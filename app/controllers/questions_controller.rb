b1a4ccb3f5f3a212b734bf0ef7c2968c85745807
Estefani Perez <estefani.perez271@gmail.com>
2022-05-23 15:58:25 +0100

    rails stupid coaching challenge done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @coach_answer = ''
+    @coach_answer = if @question == 'I am going to work right now'
+                      'Great!'
+                    elsif @question.end_with?('?')
+                      'Silly question, get dressed and go to work!'
+                    else
+                      'I don\'t care, get dressed and go to work!'
+                    end
+  end
+end
